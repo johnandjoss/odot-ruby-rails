5ce1b14386f7b253b2b627cbcdb2dfe5e6978999
jocelyn <jfrodsla@gmail.com>
2015-06-29 15:39:47 -0700

    add in scope to show all tasks that from all lists athat have been completed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
 
   def show
   	@list = List.find(params[:id])
+    @tasks = Task.done
   	render :show
   end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,5 @@
 class Task < ActiveRecord::Base
-
 	belongs_to :list
-
+	scope :done, -> { where(done: 'true') }
 	validates :description, :presence => true
 end
-
